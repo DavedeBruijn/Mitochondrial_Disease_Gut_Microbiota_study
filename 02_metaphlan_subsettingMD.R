05a8ee3761ed24d51ac7cb8e3558b1404145d400
Dave de Bruijn <dave.debruijn@radboudumc.nl>
2025-09-03 13:25:00 +0000

    Rscript used for adding metaphlan and subsetting. The metphlan table with otu and taxonomy data was added to the biom file The Mitochondrial Disease dataset was subsetted to only use the samples  at the start of the study

---
+++ 02_metaphlan_subsettingMD.R
@@ -0,0 +1,55 @@
+#Loading packages
+library("biomformat")
+library(rbiom)
+
+#adding metaphlan to the lifelines data
+#setting library paths Lifeline data
+MIDLOC_biompath_Lifeline <- "C:/Users/Z141231/OneDrive - Radboudumc/Rdata/Lifelines data/read_annotation/biom_with_taxonomy.biom"
+MIDLOC_metaphlan_tables_Lifeline <- "C:/Users/Z141231/OneDrive - Radboudumc/Rdata/Lifelines data/01_loaded/01_loaded_MIDLOCmetaphlan_tables_lifeline.tsv"
+
+# import biom files into R
+MIDLOC_biomdata_lifeline <- as_rbiom(MIDLOC_biompath_Lifeline)
+MIDLOC_biomdata_lifeline
+
+#adding otu names towards the otu's
+#import tsv files
+metaphlan_tables_lifeline <- data.table::fread(MIDLOC_metaphlan_tables_Lifeline)
+View(metaphlan_tables_lifeline)
+
+#change the otu_1 numbering towards the otu names
+MIDLOC_biomdata_lifeline$otus <- metaphlan_tables_lifeline$V1
+otu_table_lifeline <- as.matrix(MIDLOC_biomdata_lifeline$counts)
+View(otu_table_lifeline)
+
+#save rbiom object to a file
+MIDLOCMicrobiome_lifeline_savepath <- "C:/Users/Z141231/OneDrive - Radboudumc/Rdata/Lifelines data/02_metaphlan_subset/02_metaphlan_subset_lifeline.biom"
+write_biom(MIDLOC_biomdata_lifeline, MIDLOCMicrobiome_lifeline_savepath)
+
+#adding metaphlan to the MD data and subset
+# setting library paths MD data
+MIDLOC_biompath_MD <- "C:/Users/Z141231/OneDrive - Radboudumc/Rdata/MD data/01_loaded_data/01_loaded_MIDLOC.biom"
+MIDLOC_metaphlan_tables_MDpath <- "C:/Users/Z141231/OneDrive - Radboudumc/Rdata/MD data/01_loaded_data/01_loaded_MIDLOCmetaphlan_tables.tsv"
+
+# import biom files into R
+MIDLOC_biomdata_MD <- as_rbiom(MIDLOC_biompath_MD)
+MIDLOC_biomdata_MD
+
+#adding otu names towards the otu's
+#import tsv files
+MIDLOC_metaphlan_tables_MD <- data.table::fread(MIDLOC_metaphlan_tables_MDpath)
+View(MIDLOC_metaphlan_tables_MD)
+
+#change the otu_1 numbering towards the otu names
+MIDLOC_biomdata_MD$otus <- MIDLOC_metaphlan_tables_MD$V1
+otu_table_MD <- as.matrix(MIDLOC_biomdata_MD$counts)
+View(otu_table_MD)
+
+#subsetting to sample only used for the MIDLOC microbiome analysis
+metadata <- MIDLOC_biomdata_MD$metadata
+T0_samples <- metadata$.sample[grepl('T0$', metadata$.sample)]
+MIDLOC_biomdata_MD_subset <- subset(MIDLOC_biomdata_MD, MIDLOC_biomdata_MD$metadata$.sample %in% T0_samples)
+MIDLOC_biomdata_MD_subset
+
+#save rbiom object to a file
+MIDLOCMicrobiome_MD_savepath <- "C:/Users/Z141231/OneDrive - Radboudumc/Rdata/MD data/Microbiome/02_metaphlan_subset_MD/02_metaphlan_subset_MD.biom"
+write_biom(MIDLOC_biomdata_MD_subset, MIDLOCMicrobiome_MD_savepath)
