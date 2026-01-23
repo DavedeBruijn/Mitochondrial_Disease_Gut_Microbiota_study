6428b934a126e64356792c9c215b2724412605e1
Dave de Bruijn <dave.debruijn@radboudumc.nl>
2026-01-23 13:30:03 +0000

    Update file 05_DAA.R

--- 05_DAA.R
+++ 05_DAA.R
@@ -105,7 +105,7 @@ rel_abun <- as.data.frame(res$data)
 colnames(rel_abun)[1] <- ".taxa"
 
 DAA_genus <- merge(DAA_genus, rel_abun[,c(".taxa", "rel_abun")], by = ".taxa", all.x = TRUE)
-DAA_genus$rel_abun <- DAA_genus$rel_abun/sum(rel_abun$rel_abun)
+DAA_genus$rel_abun <- (DAA_genus$rel_abun/sum(DAA_genus$rel_abun))*100
 
 #making a volcano plot from the different abundance analysis Genus
 volcanoplot_results <- DAA_genus %>%
@@ -129,15 +129,15 @@ DAA_genus_plot <- ggplot(volcanoplot_results, aes(x = Log2FC_1, y = log10_adj.pv
   scale_color_gradient2(low = "blue", mid = "grey", high = "red", midpoint = 0) +
   theme_minimal()+
   labs(x = "log2 Fold Change(MD/Control)",
-       y = "-log10(p-value)",
-       size = "Relative Abundance",
+       y = "-log10(FDR)",
+       size = "Relative Abundance (%)",
        colour = "Fold Change",
        title = "Genus")+
   geom_hline(data = hline_df, aes(yintercept = yintercept), color = "blue", linetype = "dashed", linewidth = 0.5) +
   annotate("text", x = -21, y = -log10(0.035), label = "FDR < 0.05", color = "black", size = 3)+
   geom_vline(xintercept = 0, linetype = "dotted")+
   geom_point(data = top_hits, aes(color = Log2FC_1, size = rel_abun), alpha = 1)+
-  scale_size_continuous(range = c(0.1, 4), trans = "log10")+
+  scale_size_continuous(range = c(0.1, 4), trans = "log10",breaks = c(1, 0.1, 0.01, 0.001), labels = c("1", "0.1", "0.01", "0.001"))+
   geom_text_repel(data = top_hits_noGGB, aes(x = Log2FC_1, y = log10_adj.pval, label = .taxa), color = "black", size = 2.5, force = 3.5, max.overlaps = 20)
 DAA_genus_plot
 
@@ -198,7 +198,7 @@ rel_abun_species <- as.data.frame(res$data)
 colnames(rel_abun_species)[1] <- ".taxa"
 
 DAA_species <- merge(DAA_species, rel_abun_species[,c(".taxa", "rel_abun")], by = ".taxa", all.x = TRUE)
-DAA_species$rel_abun <- DAA_species$rel_abun/sum(rel_abun_species$rel_abun)
+DAA_species$rel_abun <- (DAA_species$rel_abun/sum(DAA_species$rel_abun))*100
 
 
 #making a volcano plot from the different abundance analysis Genus
@@ -223,7 +223,7 @@ DAA_species_plot <- ggplot(volcanoplot_results_species, aes(x = Log2FC_1, y = lo
   scale_color_gradient2(low = "blue", mid = "grey", high = "red", midpoint = 0) +
   theme_minimal()+
   labs(x = "log2 Fold Change(MD/Control)",
-       y = "-log10(p-value)",
+       y = "-log10(FDR)",
        size = "Relative Abundance",
        colour = "Fold Change",
        title = "Species")+
@@ -231,7 +231,7 @@ DAA_species_plot <- ggplot(volcanoplot_results_species, aes(x = Log2FC_1, y = lo
   annotate("text", x = -21, y = -log10(0.035), label = "FDR < 0.05", color = "black", size = 3)+
   geom_vline(xintercept = 0, linetype = "dotted")+
   geom_point(data = top_hits_species, aes(color = Log2FC_1, size = rel_abun), alpha = 1)+
-  scale_size_continuous(range = c(0.1, 4), trans = "log10")+
+  scale_size_continuous(range = c(0.1, 4), trans = "log10",breaks = c(1, 0.1, 0.01, 0.001), labels = c("1", "0.1", "0.01", "0.001"))+
   geom_text_repel(data = top_hits_species_noGGB, aes(x = Log2FC_1, y = log10_adj.pval, label = .taxa), color = "black", size = 2.6, force = 3.5, max.overlaps = 16)
 
 DAA_species_plot + plot_annotation(
@@ -290,3 +290,113 @@ Species_C_MD_article <- Species_C_MD_article %>%
   filter(FDR < 0.05) %>%
   arrange(FDR)
 View(Species_C_MD_article)
+
+#creating the PCoA plot with only MD and T1D
+MIDLOC_MD_T1D <- subset(MIDLOCMicrobiome_biomdata, MIDLOCMicrobiome_biomdata$metadata$group != "Control")
+MIDLOC_MD_T1D
+
+#swap the levels of the groups so that it becomes: MD control
+MIDLOC_MD_T1D$metadata$group <- factor(MIDLOC_MD_T1D$metadata$group, levels = rev(levels(MIDLOC_MD_T1D$metadata$group)))
+
+
+#Differential abundance analysis table MD_T1D
+Diff_genus_table_MD_T1D <- taxa_stats(MIDLOC_MD_T1D, rank = -2, taxa = 0.0000001, stat.by = "group", test = "wilcox", unc = "drop")
+Diff_species_table_MD_T1D <- taxa_stats(MIDLOC_MD_T1D, rank = -1, taxa = 0.0000001, stat.by = "group", test = "wilcox", unc = "drop")
+View(Diff_genus_table_MD_T1D)
+View(Diff_species_table_MD_T1D)
+
+#Adding the foldchange & relative abudance 
+Diff_genus_table_MD_T1D <- merge(Diff_genus_table_MD_T1D, DAA_genus, by = ".taxa")
+
+#making the differential abundance tables for the article
+genus_T1D_MD_article <- Diff_genus_table_MD_T1D[,-c(4,5,6,10, 11, 12,15)]
+names(genus_T1D_MD_article)[7] <- "Log2FC"
+names(genus_T1D_MD_article)[8] <- "Pvalue"
+names(genus_T1D_MD_article)[9] <- "FDR"
+
+#subsetting only the significant
+genus_T1D_MD_article <- genus_T1D_MD_article %>%
+  filter(FDR < 0.05) %>%
+  arrange(FDR)
+View(genus_T1D_MD_article)
+
+#Adding the foldchange & relative abundance
+Diff_species_table_MD_T1D <- merge(Diff_species_table_MD_T1D, DAA_species, by = ".taxa")
+
+#making the differential abundance tables for the article
+Species_T1D_MD_article <- Diff_species_table_MD_T1D[,-c(4,5,6,10, 13, 14,15)]
+names(Species_T1D_MD_article)[7] <- "Log2FC"
+names(Species_T1D_MD_article)[8] <- "Pvalue"
+names(Species_T1D_MD_article)[9] <- "FDR"
+
+#subsetting only the significant
+Species_T1D_MD_article <- Species_T1D_MD_article %>%
+  filter(FDR < 0.05) %>%
+  arrange(FDR)
+View(Species_T1D_MD_article)
+
+#making boxplots of interesting taxa
+taxa_table_genus <- taxa_table(MIDLOCMicrobiome_biomdata, rank = -2, unc = "drop", taxa = 0.000000001)
+Sphingomonas_relabund <- subset(taxa_table_genus, .taxa %in% c("Bacteroides"))
+Sphingomonas_relabund$percentage <- Sphingomonas_relabund$.abundance*100
+
+
+Sphingomonas_plot <-ggplot(Sphingomonas_relabund, aes(x = group, y = percentage, color = group))+
+  geom_violin(fill = NA, linewidth = 1.2)+
+  geom_boxplot(width = 0.3, outlier.shape = NA, alpha = 0.8)+
+  geom_jitter(width = 0.1, size = 1.5, alpha = 0.75)+
+  theme_minimal()+
+  labs(y = "Relative Abundance (%)")+
+  ggtitle("Sphingomonas")
+Sphingomonas_plot <- Sphingomonas_plot + theme(legend.position = "none")
+Sphingomonas_plot
+
+taxa_stats(MIDLOCMicrobiome_biomdata, taxa = "Sphingomonas", stat.by = "group", rank = -2, test = "wilcox", unc = "asis")
+
+#Microbacterium
+Microbacterium_relabund <- subset(taxa_table_genus, .taxa %in% c("Microbacterium"))
+Microbacterium_relabund$percentage <- Microbacterium_relabund$.abundance*100
+
+
+Microbacterium_plot <-ggplot(Microbacterium_relabund, aes(x = group, y = percentage, color = group))+
+  geom_violin(fill = NA, linewidth = 1.2)+
+  geom_boxplot(width = 0.3, outlier.shape = NA, alpha = 0.8)+
+  geom_jitter(width = 0.1, size = 1.5, alpha = 0.75)+
+  theme_minimal()+
+  labs(y = "Relative Abundance (%)")+
+  ggtitle("Microbacterium")
+Microbacterium_plot <- Microbacterium_plot + theme(legend.position = "none")
+Microbacterium_plot
+
+#GGB9597
+GGB9597_relabund <- subset(taxa_table_genus, .taxa %in% c("GGB9597"))
+GGB9597_relabund$percentage <- GGB9597_relabund$.abundance*100
+
+
+GGB9597_plot <-ggplot(GGB9597_relabund, aes(x = group, y = percentage, color = group))+
+  geom_violin(fill = NA, linewidth = 1.2)+
+  geom_boxplot(width = 0.3, outlier.shape = NA, alpha = 0.8)+
+  geom_jitter(width = 0.1, size = 1.5, alpha = 0.75)+
+  theme_minimal()+
+  labs(y = "Relative Abundance (%)")+
+  ggtitle("GGB9597")
+GGB9597_plot <- GGB9597_plot + theme(legend.position = "none")
+GGB9597_plot
+
+#extra species plots
+#c("Microbacterium_sp_T32", "Enterocloster_aldenensis", "GGB3677_SGB4990", "Clostridium_sp_Marseille_P3244", "GGB3109_SGB4121", "GGB9301_SGB14262")
+species_relabund <- subset(species_table, .taxa %in% c("Bacteroides_uniformis"))
+species_relabund$percentage <- species_relabund$.abundance*100
+
+
+species_plot <-ggplot(species_relabund, aes(x = group, y = percentage, color = group))+
+  geom_violin(fill = NA, linewidth = 1.2)+
+  geom_boxplot(width = 0.3, outlier.shape = NA, alpha = 0.8)+
+  geom_jitter(width = 0.1, size = 1.5, alpha = 0.75)+
+  theme_minimal()+
+  labs(y = "Relative Abundance (%)")+
+  ggtitle("GGB9301_SGB14262")
+species_plot <- species_plot + theme(legend.position = "none")
+species_plot
+
+taxa_stats(MIDLOCMicrobiome_biomdata, taxa = "Sphingomonas", stat.by = "group", rank = -2, test = "wilcox", unc = "asis")
